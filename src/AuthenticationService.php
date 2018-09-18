baf28c632e28d0e4c7a9ffe8442a7624e1b618ee
Joey Chen <jay.hatelove@gmail.com>
2018-09-18 20:50:38 +0800

    move classes into specific php file

--- src/AuthenticationService.php
+++ src/AuthenticationService.php
@@ -6,30 +6,60 @@
  * Time: 下午 07:43
  */
 
-namespace App;
+namespace App {
 
-class AuthenticationService
-{
-    public function isValid($account, $password)
+    class AuthenticationService
     {
-        // 根據 account 取得自訂密碼
-        $profileDao = new ProfileDao();
-        $passwordFromDao = $profileDao->getPassword($account);
-        // 根據 account 取得 RSA token 目前的亂數
-        $rsaToken = new RsaTokenDao();
-        $randomCode = $rsaToken->getRandom($account);
+        public function isValid($account, $password)
+        {
+            // 根據 account 取得自訂密碼
+            $profileDao = new ProfileDao();
+            $passwordFromDao = $profileDao->getPassword($account);
+            // 根據 account 取得 RSA token 目前的亂數
+            $rsaToken = new RsaTokenDao();
+            $randomCode = $rsaToken->getRandom($account);
 
-        var_dump($randomCode);
+            var_dump($randomCode);
 
-        // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
-        $validPassword = $passwordFromDao . $randomCode;
-        $isValid = $password === $validPassword;
+            // 驗證傳入的 password 是否等於自訂密碼 + RSA token亂數
+            $validPassword = $passwordFromDao . $randomCode;
+            $isValid = $password === $validPassword;
 
-        if ($isValid) {
-            return true;
+            if ($isValid) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
-        else {
-            return false;
+    }
+
+    class ProfileDao
+    {
+        public function getPassword($account)
+        {
+            return Context::getPassword($account);
+        }
+    }
+
+    class RsaTokenDao
+    {
+        public function getRandom($account)
+        {
+            return sprintf('%06d', mt_rand(0, 999999));
+        }
+    }
+
+    class Context
+    {
+        public static $profiles = [
+            'joey' => '91',
+            'mei' => '99',
+        ];
+
+        public static function getPassword($key)
+        {
+            return static::$profiles[$key];
         }
     }
 }
