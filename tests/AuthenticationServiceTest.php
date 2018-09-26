23f5850169480f430142675e1ee392e90484be6e
Joey Chen <jay.hatelove@gmail.com>
2018-09-26 18:28:32 +0800

    create a mock object substitute logger for assertion by mockery

--- tests/AuthenticationServiceTest.php
+++ tests/AuthenticationServiceTest.php
@@ -9,6 +9,7 @@
 namespace Tests {
 
     use App\AuthenticationService;
+    use App\ILogger;
     use App\IProfile;
     use App\IToken;
     use Mockery\MockInterface;
@@ -31,21 +32,38 @@ namespace Tests {
          * @var AuthenticationService
          */
         private $target;
+        /**
+         * @var MockInterface
+         */
+        private $mockLogger;
+        use MockeryPHPUnitIntegration;
 
         protected function setUp()
         {
             $this->stubProfile = m::mock(IProfile::class);
             $this->stubToken = m::mock(IToken::class);
-            $this->target = new AuthenticationService($this->stubProfile, $this->stubToken);
+            $this->mockLogger = m::mock(ILogger::class);
+            $this->target = new AuthenticationService($this->stubProfile, $this->stubToken, $this->mockLogger);
         }
 
-        /** @test */
-        public function is_valid_test()
+        public function test_is_valid()
         {
             $this->givenProfile('joey', '91');
             $this->givenToken('000000');
 
             $this->shouldBeValid('joey', '91000000');
+        }
+
+        public function test_log_account_when_invalid()
+        {
+            $this->givenProfile('joey', '91');
+            $this->givenToken('000000');
+
+            $this->mockLogger->shouldReceive('save')->with(m::on(function ($message) {
+                return strpos($message, 'joey') !== false;
+            }))->once();
+
+            $this->target->isValid('joey', 'wrong password');
         }
 
         private function givenProfile($account, $password)
