8df8d0b53bbeb0a568f3212086c9802dda9f64c0
Joey Chen <jay.hatelove@gmail.com>
2018-09-26 17:46:57 +0800

    create stubProfile and stubToken by mockery

--- tests/AuthenticationServiceTest.php
+++ tests/AuthenticationServiceTest.php
@@ -9,42 +9,51 @@
 namespace Tests {
 
     use App\AuthenticationService;
-    use App\FakeProfile;
-    use App\FakeToken;
+    use App\IProfile;
+    use App\IToken;
     use PHPUnit\Framework\TestCase;
+
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+    use Mockery as m;
 
     class AuthenticationServiceTest extends TestCase
     {
         /** @test */
         public function is_valid_test()
         {
-//            $target = new AuthenticationService();
-            $target = new AuthenticationService(new FakeProfile(), new FakeToken());
+            $stubProfile = m::mock(IProfile::class);
+            $stubProfile->shouldReceive('getPassword')->with('joey')->andReturn('91');
+
+            $stubToken = m::mock(IToken::class);
+            $stubToken->shouldReceive('getRandom')->andReturn('000000');
+
+//            $target = new AuthenticationService(new FakeProfile(), new FakeToken());
+            $target = new AuthenticationService($stubProfile, $stubToken);
             $actual = $target->isValid('joey', '91000000');
-            //always failed
+
             $this->assertTrue($actual);
         }
     }
 }
 
-namespace App {
-    class FakeProfile implements IProfile
-    {
-        public function getPassword($account)
-        {
-            if ($account == 'joey') {
-                return '91';
-            }
-
-            return '';
-        }
-    }
-
-    class FakeToken implements IToken
-    {
-        public function getRandom($account)
-        {
-            return '000000';
-        }
-    }
-}
+//namespace App {
+//    class FakeProfile implements IProfile
+//    {
+//        public function getPassword($account)
+//        {
+//            if ($account == 'joey') {
+//                return '91';
+//            }
+//
+//            return '';
+//        }
+//    }
+//
+//    class FakeToken implements IToken
+//    {
+//        public function getRandom($account)
+//        {
+//            return '000000';
+//        }
+//    }
+//}
