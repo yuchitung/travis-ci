1c08a16385e35e8fe8160b829e2b642ebfbe2726
Yuchi Tung <tracy770830@gmail.com>
2019-10-21 20:56:56 +0800

    refactor: make test semantic

--- tests/AuthenticationServiceTest.php
+++ tests/AuthenticationServiceTest.php
@@ -17,28 +17,70 @@ use Mockery as m;
 
 class AuthenticationServiceTest extends TestCase
 {
+    private $profile;
+    private $token;
+    private $target;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->profile = m::mock(IProfile::class);
+        $this->token = m::mock(IToken::class);
+        $this->target = new AuthenticationService($this->profile, $this->token);
+    }
+
     /** @test */
     public function is_valid()
     {
-        /**
-         * Arrange
-         */
-        $stubProfile = m::mock(IProfile::class);
-        $stubProfile->shouldReceive('getPassword')->with('joey')->andReturn('91');
-        $stubToken = m::mock(IToken::class);
-        $stubToken->shouldReceive('getRandom')->andReturn('000000');
+        $this->givenPassword('joey', '91');
+        $this->givenToken('000000');
+        $this->shouldBeValid('joey', '91000000');
+    }
 
+    /** @test */
+    public function is_invalid()
+    {
+        $this->givenPassword('joey', '91');
+        $this->givenToken('000000');
+        $this->shouldBeInvalid('joey', 'wrong password');
+    }
+
+    protected function givenPassword($account, $password): void
+    {
+        $this->profile->shouldReceive('getPassword')->with($account)->andReturn($password);
+    }
+
+    protected function givenToken($token): void
+    {
+        $this->token->shouldReceive('getRandom')->andReturn($token);
+    }
+
+    protected function shouldBeValid($account, $password): void
+    {
         /**
          * Act
          */
-        $target = new AuthenticationService($stubProfile, $stubToken);
-        $actual = $target->isValid('joey', '91000000');
+        $actual = $this->target->isValid($account, $password);
 
         /**
          * Assert
          */
         $this->assertTrue($actual);
     }
+
+    protected function shouldBeInvalid($account, $password): void
+    {
+        /**
+         * Act
+         */
+        $actual = $this->target->isValid($account, $password);
+
+        /**
+         * Assert
+         */
+        $this->assertFalse($actual);
+    }
+
 
 }
 
