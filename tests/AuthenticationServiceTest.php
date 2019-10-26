900afa5952a7215d55923a0ecc5286adf00840bf
Yuchi Tung <tracy770830@gmail.com>
2019-10-27 00:38:47 +0800

    test: add test:should_log_account_when_invalid

--- tests/AuthenticationServiceTest.php
+++ tests/AuthenticationServiceTest.php
@@ -9,6 +9,7 @@
 namespace Tests;
 
 use App\AuthenticationService;
+use App\ILogger;
 use App\IProfile;
 use App\IToken;
 use PHPUnit\Framework\TestCase;
@@ -20,13 +21,15 @@ class AuthenticationServiceTest extends TestCase
     private $profile;
     private $token;
     private $target;
+    private $mockLogger;
 
     protected function setUp()
     {
         parent::setUp();
         $this->profile = m::mock(IProfile::class);
         $this->token = m::mock(IToken::class);
-        $this->target = new AuthenticationService($this->profile, $this->token);
+        $this->mockLogger = m::mock(ILogger::class);
+        $this->target = new AuthenticationService($this->profile, $this->token, $this->mockLogger);
     }
 
     /** @test */
@@ -38,11 +41,16 @@ class AuthenticationServiceTest extends TestCase
     }
 
     /** @test */
-    public function is_invalid()
+    public function should_log_account_when_invalid()
     {
         $this->givenPassword('joey', '91');
         $this->givenToken('000000');
-        $this->shouldBeInvalid('joey', 'wrong password');
+
+        $this->mockLogger->shouldReceive('save')->with(m::on(function ($message) {
+            return strpos($message, 'joey') !== false;
+        }))->once();
+
+        $this->target->isValid('joey', 'wrong password');
     }
 
     protected function givenPassword($account, $password): void
@@ -57,30 +65,9 @@ class AuthenticationServiceTest extends TestCase
 
     protected function shouldBeValid($account, $password): void
     {
-        /**
-         * Act
-         */
         $actual = $this->target->isValid($account, $password);
-
-        /**
-         * Assert
-         */
         $this->assertTrue($actual);
     }
-
-    protected function shouldBeInvalid($account, $password): void
-    {
-        /**
-         * Act
-         */
-        $actual = $this->target->isValid($account, $password);
-
-        /**
-         * Assert
-         */
-        $this->assertFalse($actual);
-    }
-
 
 }
 
